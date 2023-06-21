a246ada2aa13b8b58d8b380d0bb7f64b69ff305c
thalderg <33346659+thalderg@users.noreply.github.com>
2023-06-21 16:33:34 +0200

    Update HelloWorld1.sol

--- contracts/HelloWorld1.sol
+++ contracts/HelloWorld1.sol
@@ -1,4 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 
+
+contract HelloWorld {
+    string private text;
+    address public owner;
+
+    constructor() {
+        text = pureText();
+        owner = msg.sender;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Sender is not the owner");
